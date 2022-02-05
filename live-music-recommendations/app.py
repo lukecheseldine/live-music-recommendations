ab5477564c972f72cb5179aa917df672bb203cc9
lukecheseldine <cheeseldine@gmail.com>
2022-02-05 14:21:52 -0500

    reset

--- live-music-recommendations/app.py
+++ live-music-recommendations/app.py
@@ -1,20 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
     return render_template('index.html')
-
-@app.route('/lol')
-def test():
-    pass
-=======
-    return 'Hello, World!'
-
-
-def tester():
-    pass
->>>>>>> fcd780a8cc8e421913b9dcfd3715f7fd852943ee
