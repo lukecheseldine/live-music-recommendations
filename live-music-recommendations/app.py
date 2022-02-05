a3e0e40839116b9f4a410f4388d024d307b6e68f
Rohit Prakash <rohit.prakash@mail.utoronto.ca>
2022-02-05 14:17:45 -0500

    testing git

--- live-music-recommendations/app.py
+++ live-music-recommendations/app.py
@@ -5,16 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
     return render_template('index.html')
+
 
 @app.route('/lol')
 def test():
     pass
-=======
     return 'Hello, World!'
 
 
 def tester():
     pass
->>>>>>> fcd780a8cc8e421913b9dcfd3715f7fd852943ee
