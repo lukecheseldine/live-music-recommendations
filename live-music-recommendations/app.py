ebc752934b8e3086ec69d835f6bde000b01f62f4
lukecheseldine <cheeseldine@gmail.com>
2022-02-05 22:33:59 -0500

    temp

--- live-music-recommendations/app.py
+++ live-music-recommendations/app.py
@@ -5,17 +5,21 @@ from flask_session import Session
 
 
 app = Flask(__name__)
+
 # session for storing access_token
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
 # spotify api credentials
-CLIENT_ID = '6151954dbc3842b98c6f8bbab8616310'
-CLIENT_SECRET = '43e0561d85b944de8c6e9059d9a8a7f1'
-REDIRECT_URI = 'http://127.0.0.1:5000/callback'
-SCOPES = 'user-read-private user-follow-read user-library-read playlist-read-private user-top-read'
+SPOTIFY_CLIENT_ID = '6151954dbc3842b98c6f8bbab8616310'
+SPOTIFY_CLIENT_SECRET = '43e0561d85b944de8c6e9059d9a8a7f1'
+SPOTIFY_REDIRECT_URI = 'http://127.0.0.1:5000/callback'
+SPOTIFY_SCOPES = 'user-read-private user-follow-read user-library-read playlist-read-private user-top-read'
 
-
+# seatgeek api credentials
+SEATGEEK_CLIENT_ID = 'MjU2MTgyNzV8MTY0NDExNzE3OS4yNjMwMjk'
+SEATGEEK_CLIENT_SECRET = '8b866a86d6e0a65707b99864a3913c51b75431e62261c2f814611abce7c6f5b0'
 
 @app.route('/')
 def index():
@@ -24,7 +28,7 @@ def index():
 
 @app.route('/authorize', methods = ['POST', 'GET'])
 def authorize():
-    url = f'https://accounts.spotify.com/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPES}'
+    url = f'https://accounts.spotify.com/authorize?response_type=code&client_id={SPOTIFY_CLIENT_ID}&redirect_uri={SPOTIFY_REDIRECT_URI}&scope={SPOTIFY_SCOPES}'
     return redirect(url)
     
 
@@ -38,10 +42,10 @@ def callback():
         url = f'https://accounts.spotify.com/api/token'
         response = post(url, data={
             'code': code,
-            'redirect_uri': REDIRECT_URI,
+            'redirect_uri': SPOTIFY_REDIRECT_URI,
             'grant_type': 'authorization_code'
         }, headers={ \
-            'Authorization': 'Basic ' + (urlsafe_b64encode(str.encode(CLIENT_ID + ':' + CLIENT_SECRET)).decode()),
+            'Authorization': 'Basic ' + (urlsafe_b64encode(str.encode(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET)).decode()),
             'Content-Type': 'application/x-www-form-urlencoded'
         }) 
         response_data = response.json()
@@ -57,7 +61,8 @@ def recommendation():
 
 @app.route('/artist', methods=['POST', 'GET'])
 def artist():
-    return f"{request.form.get('selection')}"
+    return render_template('artist.html')
+    
 
 
 @app.route('/genre', methods=['POST', 'GET'])
@@ -74,3 +79,31 @@ def genre():
 @app.route('/playlist', methods=['POST', 'GET'])
 def playlist():
     return f"{request.form.get('selection')}"
+
+@app.route('/results/genre/<genre>', methods=['POST', 'GET'])
+def results(genre):
+    url = 'https://api.spotify.com/v1/recommendations'
+    params = {
+        'seed_genres': str(genre),
+        'limit': 50
+    }
+    headers = {
+        'Authorization': f'Bearer {session.get("access_token")}'
+    }
+    response = get(url, params=params, headers=headers)
+    response_data = response.json()
+
+    artists = set()
+    for i in range(len(response_data['tracks'])):
+        name = response_data['tracks'][i]['album']['artists'][0]['name']
+        if name != 'Various Artists':
+            artists.add(name)
+        
+    # remove duplicates
+    artists = list(artists)
+
+    url = 
+    
+
+
+    return f"{artists} {len(artists)}"
