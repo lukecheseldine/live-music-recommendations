54b0f9e0d40e0d78c2a3d8c58b5e79d673e69857
lukecheseldine <cheeseldine@gmail.com>
2022-02-05 20:46:43 -0500

    backend for recommendation and genre partially

--- live-music-recommendations/app.py
+++ live-music-recommendations/app.py
@@ -1,10 +1,14 @@
-from crypt import methods
-from flask import Flask, render_template, request, redirect
-from requests import post
+from flask import Flask, render_template, request, redirect, session
+from requests import post, get
 from base64 import urlsafe_b64encode
+from flask_session import Session
 
 
 app = Flask(__name__)
+# session for storing access_token
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 # spotify api credentials
 CLIENT_ID = '6151954dbc3842b98c6f8bbab8616310'
 CLIENT_SECRET = '43e0561d85b944de8c6e9059d9a8a7f1'
@@ -18,10 +22,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/authorize', methods = ['POST'])
+@app.route('/authorize', methods = ['POST', 'GET'])
 def authorize():
     url = f'https://accounts.spotify.com/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope={SCOPES}'
-    print(url)
     return redirect(url)
     
 
@@ -41,11 +44,32 @@ def callback():
             'Authorization': 'Basic ' + (urlsafe_b64encode(str.encode(CLIENT_ID + ':' + CLIENT_SECRET)).decode()),
             'Content-Type': 'application/x-www-form-urlencoded'
         }) 
+        response_data = response.json()
+        access_token = response_data['access_token']
+        session['access_token'] = access_token
         return redirect('/recommendation')
+
 
 @app.route('/recommendation')
 def recommendation():
     return render_template('recommendation.html')
+
+
+@app.route('/artist', methods=['POST', 'GET'])
+def artist():
+    return f"{request.form.get('selection')}"
+
+
+@app.route('/genre', methods=['POST', 'GET'])
+def genre():
+    url = 'https://api.spotify.com/v1/recommendations/available-genre-seeds'
+    headers = {
+        'Authorization': f'Bearer {session.get("access_token")}'
+    }
+    response = get(url, headers=headers)
+    genres = response.json()['genres']
+    return render_template('genre.html', genres=genres)
+
 
 @app.route('/playlist', methods=['POST', 'GET'])
 def playlist():
--- live-music-recommendations/static/recommendation.js
+++ live-music-recommendations/static/recommendation.js
@@ -1,3 +1,12 @@
+form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+    console.log(e.target.getAttribute('action'))
+    if (!e.target.getAttribute('action')) {
+        e.preventDefault();
+        displayError()
+    }
+}, true)
+
 playlistButton = document.getElementById('playlist');
 playlistButton.addEventListener('click', buttonPressed);
 
@@ -10,6 +19,12 @@ genreButton.addEventListener('click', buttonPressed);
 selectionInput = document.getElementById('selection');
 
 function buttonPressed(e) {
-    selection = e.target.innerText
+    selection = e.target.innerText.toLowerCase()
     selectionInput.setAttribute('value', selection)
+    form.setAttribute('action', `/${selection}`)
+}
+
+function displayError() {
+    // TODO
+    console.log('make selection')
 }
